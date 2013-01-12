0f8bf57ab1a5bb2dc782ae8039c4638f7a0ea677
lemonjia <angersax@gmail.com>
2013-01-12 10:04:48 +0800

    [yafuse]: fix error of compiling.

---
+++ yafuse/libfat32/unix_io.c
@@ -0,0 +1,31 @@
+/**
+ * unix_io.c - UNIX io interface.
+ *
+ * Copyright (c) 2013-2014 angersax@gmail.com
+ *
+ * This program/include file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program/include file is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (in the main directory of the NTFS-3G
+ * distribution in the file COPYING); if not, write to the Free Software
+ * Foundation,Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "include/libfat32/debug.h"
+
+#ifdef DEBUG
+// Add code here
+#endif
+
